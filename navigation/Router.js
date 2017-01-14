52eb88be90c411fc93d9007409c0c6a915f02b24
Matt Conflitti <conflittimat@gmail.com>
2017-01-14 16:31:38 -0500

    Added splash screen.

--- navigation/Router.js
+++ navigation/Router.js
@@ -2,12 +2,14 @@ import {
   createRouter,
 } from '@exponent/ex-navigation';
 
+import SplashScreen from '../screens/SplashScreen';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import RootNavigation from './RootNavigation';
 
 export default createRouter(() => ({
+  splash: () => SplashScreen,
   home: () => HomeScreen,
   links: () => LinksScreen,
   settings: () => SettingsScreen,
---
+++ screens/SplashScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+import Router from '../navigation/Router';
+
+class SplashScreen extends Component {
+    componentWillMount () {
+        let navigator = this.props.navigator;
+        setTimeout (() => {
+            navigator.replace(Router.getRoute('home'));
+        }, 3000);
+    }
+
+    render () {
+        return (
+            <View style={{flex: 1, backgroundColor: '#f2f2f2', alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    style={{flex: 1,
+                        width: 200,
+                        height: 50,
+                        resizeMode: 'contain'}}
+                    source={require('../assets/images/logo.png')}
+                />
+            </View>
+        );
+    }
+}
+
+export default SplashScreen;
